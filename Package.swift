51b208f7fdc5d4c7844a4eedc8827fa86ba3e652
Andrew Waters <waters@me.com>
2024-10-27 22:22:40 +0000

    Remove keychain

--- Package.swift
+++ Package.swift
@@ -12,9 +12,8 @@ let package = Package(
             targets: ["CtrlHubAPI"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/kishikawakatsumi/KeychainAccess.git", from: "3.0.0"),
-        .package(url: "https://github.com/DataDog/swift-jsonapi.git", from: "0.1.1"),
-        .package(url: "https://github.com/Alamofire/Alamofire.git", from: "5.10.1")
+        .package(url: "https://github.com/Alamofire/Alamofire.git", from: "5.10.1"),
+        .package(url: "https://github.com/DataDog/swift-jsonapi.git", from: "0.1.1")
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
--- Source/Features/Models/ServiceAccount.swift
+++ Source/Features/Models/ServiceAccount.swift
@@ -6,7 +6,7 @@
 //
 
 import Foundation
-//import JSONAPI
+
 
 @ResourceWrapper(type: "service-accounts")
 public struct ServiceAccount: Sendable, Equatable {
