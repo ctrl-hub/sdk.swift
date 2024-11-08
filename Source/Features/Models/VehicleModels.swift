21288e308f40fdef6d198bd74271d34d351b3bda
Andrew Waters <waters@me.com>
2024-11-08 16:32:33 +0000

    Adds vehicle list

---
+++ Source/Features/Models/VehicleModels.swift
@@ -0,0 +1,23 @@
+// Unless explicitly stated otherwise all files in this repository are licensed under
+// the MIT License.
+//
+// This product includes software developed at Ctrl Hub (https://www.ctrl-hub.com).
+// Copyright 2024-Present Ctrl Hub Limited.
+
+import Foundation
+import JSONAPI
+
+// MARK: - Resources
+
+@ResourceWrapper(type: "vehicles")
+public struct Vehicle: Sendable, Equatable, Identifiable {
+    public var id: String
+
+    @ResourceAttribute() public var vin: String
+
+    @ResourceAttribute() public var registration: String?
+
+    @ResourceAttribute() public var description: String?
+
+    @ResourceAttribute() public var emissions: Float64?
+}
---
+++ Source/Features/Routers/VehiclesRouter.swift
@@ -0,0 +1,42 @@
+// Unless explicitly stated otherwise all files in this repository are licensed under
+// the MIT License.
+//
+// This product includes software developed at Ctrl Hub (https://www.ctrl-hub.com).
+// Copyright 2024-Present Ctrl Hub Limited.
+
+import Foundation
+import JSONAPI
+
+// MARK: - Router
+
+enum VehiclesRouter: Route {
+    case All(String)
+
+    var path: String {
+        switch self {
+        case .All(let orgId):
+            return "/v3/orgs/\(orgId)/assets/vehicles"
+        }
+    }
+    
+    var method: String {
+        switch self {
+        case .All:
+            return "GET"
+        }
+    }
+}
+
+// MARK: - Interface
+
+public actor Vehicles {
+
+    @MainActor public static let shared = Vehicles()
+    private init() {}
+    private let decoder = JSONAPIDecoder()
+
+    public func Get(orgId: String) async throws -> [Vehicle] {
+        let (data, response) = try await VehiclesRouter.All(orgId).Request()
+        return try decoder.decode([Vehicle].self, from: data)
+    }
+}
